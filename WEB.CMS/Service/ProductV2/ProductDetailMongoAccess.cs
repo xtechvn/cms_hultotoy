868abef6ccc7c6d465c0932172c4dcf9281b7e49
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2024-08-09 11:06:02 +0700

    add product mongodb access
    add product mongodb access

---
+++ WEB.CMS/Service/ProductV2/ProductDetailMongoAccess.cs
@@ -0,0 +1,123 @@
+﻿using Entities.ViewModels;
+using Entities.ViewModels.Product;
+using Entities.ViewModels.Products;
+using Entities.ViewModels.Products.V2;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+
+namespace WEB.CMS.Models.Product.V2
+{
+    public class ProductDetailMongoAccess
+    {
+        private readonly IConfiguration _configuration;
+        private IMongoCollection<ProductMongoDbModel> _productDetailCollection;
+
+        public ProductDetailMongoAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            string url = "mongodb://" + configuration["DataBaseConfig:MongoServer:Host"] + "";
+            var client = new MongoClient("mongodb://" + configuration["DataBaseConfig:MongoServer:Host"] + "");
+            IMongoDatabase db = client.GetDatabase(configuration["DataBaseConfig:MongoServer:catalog"]);
+            _productDetailCollection = db.GetCollection<ProductMongoDbModel>("ProductDetail");
+        }
+        public async Task<string> AddNewAsync(ProductMongoDbModel model)
+        {
+            try
+            {
+                await _productDetailCollection.InsertOneAsync(model);
+                return model._id;
+            }
+            catch (Exception ex)
+            {
+                Utilities.LogHelper.InsertLogTelegram("ProductDetailMongoAccess - AddNewAsync: \nData: aff_model: " + JsonConvert.SerializeObject(model) + ".\n Error: " + ex);
+                return null;
+            }
+        }
+        public async Task<string> UpdateAsync(ProductMongoDbModel model)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.And(
+                    filter.Eq("_id", model._id));
+                await _productDetailCollection.FindOneAndReplaceAsync(filterDefinition, model);
+                return model._id;
+            }
+            catch (Exception ex)
+            {
+                Utilities.LogHelper.InsertLogTelegram("ProductDetailMongoAccess - UpdateAsync: \nData: aff_model: " + JsonConvert.SerializeObject(model) + ".\n Error: " + ex);
+                return null;
+            }
+        }
+        public async Task<string> DeleteAsync(ProductBlackList model)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.And(
+                    filter.Eq("_id", model._id)
+                    );
+                await _productDetailCollection.FindOneAndDeleteAsync(filterDefinition);
+                return model._id;
+            }
+            catch (Exception ex)
+            {
+                Utilities.LogHelper.InsertLogTelegram("ProductDetailMongoAccess - DeleteAsync: \nData: aff_model: " + JsonConvert.SerializeObject(model) + ".\n Error: " + ex);
+                return null;
+            }
+        }
+        public async Task<string> FindIDByProductCode(string product_code)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.Empty;
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.Eq(x => x.code, product_code); ;
+                var model = await _productDetailCollection.Find(filterDefinition).FirstOrDefaultAsync();
+                if(model!=null && model._id!=null && model._id.Trim()!="")
+                return model._id;
+            }
+            catch (Exception ex)
+            {
+                Utilities.LogHelper.InsertLogTelegram("ProductDetailMongoAccess - FindIDByProductCode with [ "+product_code+" ] Error: " + ex);
+            }
+            return null;
+
+        }
+        public async Task<ProductMongoDbModel> FindByID(string id)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.Empty;
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.Eq(x => x._id, id); ;
+                var model = await _productDetailCollection.Find(filterDefinition).FirstOrDefaultAsync();
+                return model;
+            }
+            catch (Exception ex)
+            {
+                Utilities.LogHelper.InsertLogTelegram("ProductDetailMongoAccess - FindByID Error: " + ex);
+                return null;
+            }
+        }
+        public async Task<ProductMongoDbModel> FindDetailByProductCode(string product_code)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.Empty;
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.Eq(x => x.code, product_code); ;
+                var model = await _productDetailCollection.Find(filterDefinition).FirstOrDefaultAsync();
+                return model;
+            }
+            catch (Exception ex)
+            {
+                Utilities.LogHelper.InsertLogTelegram("ProductDetailMongoAccess - FindDetailByProductCode Error: " + ex);
+                return null;
+            }
+        }
+      
+      
+     
+    }
+}
