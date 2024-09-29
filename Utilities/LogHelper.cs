532c1bc28f4cb272b81068f7b3ca304154efc8a1
Đăng Tây <nguyendang01092003@gmail.com>
2024-09-29 20:56:50 +0700

    UpdateMessageQueue

--- Utilities/LogHelper.cs
+++ Utilities/LogHelper.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Telegram.Bot;
@@ -18,7 +19,7 @@ namespace Utilities
         public static string CompanyType = " ";
         public static int CompanyTypeInt = 0;
 
-       
+
         private static void LoadConfig()
         {
 
@@ -31,7 +32,7 @@ namespace Utilities
                 botToken = _appconfig.BotSetting.bot_token;
                 group_Id = _appconfig.BotSetting.bot_group_id;
                 string company_type = _appconfig.CompanyType;
-                if (company_type!=null && company_type.Trim()!="")
+                if (company_type != null && company_type.Trim() != "")
                 {
                     CompanyTypeInt = Convert.ToInt32(company_type);
                     switch (CompanyTypeInt)
@@ -62,7 +63,7 @@ namespace Utilities
             {
                 LoadConfig();
                 TelegramBotClient alertMsgBot = new TelegramBotClient(botToken);
-                var rs_push=  alertMsgBot.SendTextMessageAsync(group_Id, "[" + enviromment + "-"+CompanyType+"] - " + message).Result;
+                var rs_push = alertMsgBot.SendTextMessageAsync(group_Id, "[" + enviromment + "-" + CompanyType + "] - " + message).Result;
             }
             catch (Exception ex)
             {
@@ -70,6 +71,69 @@ namespace Utilities
             }
             return rs;
         }
+
+        public static void InsertLogTelegramByUrl(string bot_token, string id_group, string msg)
+        {
+            string JsonContent = string.Empty;
+            string url_api = "https://api.telegram.org/bot" + bot_token + "/sendMessage?chat_id=" + id_group + "&text=" + msg;
+            try
+            {
+                using (var webclient = new WebClient())
+                {
+                    JsonContent = webclient.DownloadString(url_api);
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLogActivity("D://", ex.ToString());
+            }
+        }
+        public static void WriteLogActivity(string AppPath, string log_content)
+        {
+            StreamWriter sLogFile = null;
+            try
+            {
+                //Ghi lại hành động của người sử dụng vào log file
+                string sDay = string.Format("{0:dd}", DateTime.Now);
+                string sMonth = string.Format("{0:MM}", DateTime.Now);
+                string strLogFileName = sDay + "-" + sMonth + "-" + DateTime.Now.Year + ".log";
+                string strFolderName = AppPath + @"\Logs\" + DateTime.Now.Year + "-" + sMonth;
+                //Application.StartupPath
+                //Tạo thư mục nếu chưa có
+                if (!Directory.Exists(strFolderName + @"\"))
+                {
+                    Directory.CreateDirectory(strFolderName + @"\");
+                }
+                strLogFileName = strFolderName + @"\" + strLogFileName;
+
+                if (File.Exists(strLogFileName))
+                {
+                    //Nếu đã tồn tại file thì tiếp tục ghi thêm
+                    sLogFile = File.AppendText(strLogFileName);
+                    sLogFile.WriteLine(string.Format("Thời điểm ghi nhận: {0:hh:mm:ss tt}", DateTime.Now));
+                    sLogFile.WriteLine(string.Format("Chi tiết log: {0}", log_content));
+                    sLogFile.WriteLine("-------------------------------------------");
+                    sLogFile.Flush();
+                }
+                else
+                {
+                    //Nếu file chưa tồn tại thì có thể tạo mới và ghi log
+                    sLogFile = new StreamWriter(strLogFileName);
+                    sLogFile.WriteLine(string.Format("Thời điểm ghi nhận: {0:hh:mm:ss tt}", DateTime.Now));
+                    sLogFile.WriteLine(string.Format("Chi tiết log: {0}", log_content));
+                    sLogFile.WriteLine("-------------------------------------------");
+                }
+                sLogFile.Close();
+            }
+            catch (Exception)
+            {
+                if (sLogFile != null)
+                {
+                    sLogFile.Close();
+                }
+            }
+        }
+        /*
         /*
         /// <summary>
         /// function ghi log vao telegram
@@ -181,7 +245,7 @@ namespace Utilities
     {
         public BotSetting BotSetting { get; set; }
         public string CompanyType { get; set; }
-      
+
     }
 
     public class BotSetting
